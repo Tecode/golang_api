0058540590d63fefec4855ec3271f4fb259120ab
Tecode <283731869@qq.com>
2019-05-21 23:45:02 +0800

    '获取图片宽高'

--- helpers/compressPicture.go
+++ helpers/compressPicture.go
@@ -2,8 +2,11 @@ package helpers
 
 import (
 	"bytes"
+	"errors"
+	"fmt"
 	"github.com/astaxie/beego/context"
 	"github.com/nfnt/resize"
+	"image"
 	"image/jpeg"
 	"log"
 	"os"
@@ -11,6 +14,7 @@ import (
 
 func CompressPicture(ctx *context.Context) {
 	var buf bytes.Buffer
+	size := 170
 	file, err := os.Open("resource/images/6Co6W3DogOqW6TxwDp8Vb.jpg")
 	if err != nil {
 		log.Fatal(err)
@@ -20,10 +24,34 @@ func CompressPicture(ctx *context.Context) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	m := resize.Resize(400, 200, img, resize.Bicubic)
-	// write new image to file
-	jpeg.Encode(&buf, m, nil)
+	b := img.Bounds()
+	width := b.Max.X
+	height := b.Max.Y
+	fmt.Println(width,height, "图片宽高----------")
+	// 压缩图片
+	m := resize.Resize(200, 0, img, resize.Bicubic)
+	// 裁剪图片
+	curImage, _ := ImageCopy(m, 0, 0, size, size)
+	jpeg.Encode(&buf, curImage, nil)
 	file.Close()
 	data := buf.Bytes()
 	ctx.Output.Body(data)
+}
+
+func ImageCopy(src image.Image, x, y, w, h int) (image.Image, error) {
+
+	var subImg image.Image
+
+	if rgbImg, ok := src.(*image.YCbCr); ok {
+		subImg = rgbImg.SubImage(image.Rect(x, y, x+w, y+h)).(*image.YCbCr) //图片裁剪x0 y0 x1 y1
+	} else if rgbImg, ok := src.(*image.RGBA); ok {
+		subImg = rgbImg.SubImage(image.Rect(x, y, x+w, y+h)).(*image.RGBA) //图片裁剪x0 y0 x1 y1
+	} else if rgbImg, ok := src.(*image.NRGBA); ok {
+		subImg = rgbImg.SubImage(image.Rect(x, y, x+w, y+h)).(*image.NRGBA) //图片裁剪x0 y0 x1 y1
+	} else {
+
+		return subImg, errors.New("图片解码失败")
+	}
+
+	return subImg, nil
 }
