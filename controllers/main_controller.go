e71cfce769dd61fbd207ada0912eba93a14cd699
Tecode <283731869@qq.com>
2023-06-24 13:23:09 +0800

    新增页面

--- controllers/main_controller.go
+++ controllers/main_controller.go
@@ -19,3 +19,18 @@ func (c *MainController) Get() {
 func CertificateText(ctx *context.Context) {
 	ctx.Output.Download("./resource/certificate/1BA546375A5C77B851DB153B5516819B.txt")
 }
+
+// Contact 联系我们
+func (c *MainController) Contact() {
+	c.TplName = "contact.html"
+}
+
+// News 新闻资讯
+func (c *MainController) News() {
+	c.TplName = "news.html"
+}
+
+// NewsDetail 新闻资讯详情
+func (c *MainController) NewsDetail() {
+	c.TplName = "news-detail.html"
+}
--- routers/router.go
+++ routers/router.go
@@ -24,8 +24,12 @@ func init() {
 	// 包含token验证，中间件过滤拦截
 	beego.InsertFilter("/api/v1/user/*", beego.BeforeRouter, utils.Interceptor)
 
-	// http://localhost:8080/haoxuan
+	// http://localhost:8080/contact
+	// 模板引擎页面
 	beego.Router("/", &controllers.MainController{})
+	beego.CtrlGet("/contact", (*controllers.MainController).Contact)
+	beego.CtrlGet("/news", (*controllers.MainController).News)
+	beego.CtrlGet("/news-detail", (*controllers.MainController).NewsDetail)
 	beego.Get("/.well-known/pki-validation/1BA546375A5C77B851DB153B5516819B.txt", controllers.CertificateText)
 	beego.AddNamespace(nameSpace)
 }
